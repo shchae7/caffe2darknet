00b2695e5a3bee9f87db7003c080fb6444bc0241
shchae7 <shchae7@postech.ac.kr>
2019-08-10 14:38:10 +0900

    weightParser main updated

--- parser/weightsParser.c
+++ parser/weightsParser.c
@@ -3,8 +3,14 @@ void parse_weights(char *datacfg, char *cfgfile, char *weightfile)
     network *net = load_network(cfgfile, weightfile, 0);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    parse_weights("/home/shchae7/
+    if(argc == 4)
+    {
+        parse_weights(argv[1], argv[2], argv[3]);
+        return 0;
+    }
+
+    fprinf(stderr, "try: ./weightParser cfg/test.data cfg/test.cfg weights/test.weights\n")
     return 0;
 }
